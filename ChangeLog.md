cef11d1dbabfd8c0cd47af4d43cb75319dcd488a
burstknight <burstknight@gmail.com>
2022-07-09 15:26:25 +0800

    feat: Add head file includes/LoggingCpp.h
    Add head file includes/LoggingCpp.h.

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,6 +2,7 @@
 
 ## [Unrelease]
 ### [Added]
+- Add the head file `includes/LoggingCpp.h` to define the namespace and class.
 
 ### [Changed]
 
---
+++ includes/LoggingCpp.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace LoggingCpp {
+	enum LoggingLevel{
+		Debug,
+		Info,
+		Warning,
+		Error,
+		Critical
+	};	// End of enum LoggingLevel
+
+	class myLogger{
+		// private fields
+	private:
+		char *m_pcLogPath;
+
+		// public methods
+	public:
+		/* Description: Constructor
+		 * @param pcLogPath: The path of the log file.
+		 * */
+		myLogger(const char *pcLogPath);
+
+		/* Description: Log message.
+		 * @param level: The log level.
+		 * @param pcKeyword: The keyword of the log message.
+		 * @param pcMessage: The message to log.
+		 * @param isWrite: Write the message into file if this flag is `true`, otherwise just show on terminal
+		 * @return return 0 if succeed to log message, otherwise return 1
+		 * */
+		int logMessage(LoggingLevel level, const char *pcKeyword, bool isWrite ,const char *pcMessage, ...);
+	};	// End of class myLogger
+
+}	// En of namespace LoggingCpp
